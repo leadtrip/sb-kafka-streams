5f22caa75387f4660325976735989d36069872a8
leadtrip <m1k3w000d@gmail.com>
2026-03-08 09:58:40 +0000

    Initial commit

---
+++ consumer-service-a/src/main/java/wood/mike/sbkafkastreams/SbKafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package wood.mike.sbkafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKafkaStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbKafkaStreamsApplication.class, args);
+    }
+
+}
---
+++ consumer-service-b/src/test/java/wood/mike/sbkafkastreams/SbKafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package wood.mike.sbkafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbKafkaStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
